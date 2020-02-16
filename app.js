973fc2f6b69d5fd1aad9d38fdc635bf5a116ebe9
Alex Getz <asgetz@student.ysu.edu>
2020-02-15 21:44:01 -0500

    Login works now in addition to create profile

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var currentProfile;
+
 let itemsArray = localStorage.getItem('items') ?
 	JSON.parse(localStorage.getItem('items')) : [];
 
@@ -12,7 +14,11 @@ class userProfile{
 		itemsArray.push(this.email+","+this.psw);
 		localStorage.setItem('items',JSON.stringify(itemsArray));
 	}
-	setActive(){}
+
+	setActive(){
+		this.active = true;
+	}
+
 	setInactive(){}
 	getItem(){}
 	removeItem(){}
@@ -21,4 +27,10 @@ class userProfile{
 	static fromJSON(serializedJson){
 		return Object.assign(new userProfile(),JSON.parse(serializedJson));
 	}
+}
+
+function profileSet(email,psw){
+	currentProfile = new userProfile(email,psw);
+	currentProfile.setActive();
+	// alert("Profile set!");
 }
